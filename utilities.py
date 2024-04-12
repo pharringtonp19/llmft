66f7c0eaabece986d8ca383c7fe80f70d916b75f
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-12 19:20:29 -0400

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -7,4 +7,17 @@ def get_special_tokens(tokenizer):
 
 def get_decoding(tokenizer, model_inputs):
     return tokenizer.batch_decode(model_inputs.input_ids)
+
+
+
+
+### ---         Importing PyTorch
+import torch
+import torch.nn.functional as F
+print(f"PyTorch Version: {torch.__version__}")
+
+import torch
+print(f"Cude is available: {torch.cuda.is_available()}")
+print(f"Device name: {torch.cuda.get_device_name(0)}")
+### ---
     
