3d0c3c9878a9176ac99bc4a35ac026f527b7ba21
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-12 08:36:22 -0400

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
-def cls_sep_pad(tokenizer):
+def get_special_tokens(tokenizer):
     cls = [tokenizer.cls_token, tokenizer.cls_token_id]
     sep = [tokenizer.sep_token, tokenizer.sep_token_id]
     pad = [tokenizer.pad_token, tokenizer.pad_token_id]
-    print(cls, sep, pad)
+    bos = [tokenizer.bos_token, tokenizer.bos_token_id]
+    print(cls, sep, pad, bos)
