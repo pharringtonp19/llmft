005bd21cb8c28a5e56a1b643f3928ae7cd5f941f
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-14 07:38:27 -0400

    Update setup_env.sh

--- getting-started/setup_env.sh
+++ getting-started/setup_env.sh
@@ -14,7 +14,7 @@ source $ENV_NAME/bin/activate
 
 # Install any necessary Python packages
 pip3 install torch torchvision torchaudio
-pip install transformers datasets accelerate peft bitsandbytes ipywidgets evaluate 
+pip install transformers datasets accelerate peft bitsandbytes ipywidgets evaluate huggingface_hub
 pip install matplotlib
 
 echo "Setup complete and the virtual environment $ENV_NAME is ready to use."
