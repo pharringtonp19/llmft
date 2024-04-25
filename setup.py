c05e5f0952e1775d4a3168997f2e20a90049dd53
pharringtonp19 <pharringtonp19@gmail.com>
2024-04-25 17:23:09 +0000

    updates

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llmft',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list libraries needed by your library to work
+    ],
+    author='Patrick Power',
+    author_email='pharringtonp19@gmai.com',
+    description='fine-tuning LLMs for Causal Inference Tasks',
+    license='MIT',
+    keywords='example project',
+)
