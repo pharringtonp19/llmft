0f24a9da6690efdac956f6ba714bdfeac5555004
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-26 07:24:28 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trics',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your project's dependencies here
+        # e.g., 'requests>=2.23.0'
+    ],
+)
