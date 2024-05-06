38e88ab82691e6914a677fb7b55937d145327098
pharringtonp19 <pharringtonp19@gmail.com>
2024-05-06 21:22:10 +0000

    updates

--- getting-started/setup_env.sh
+++ getting-started/setup_env.sh
@@ -16,7 +16,7 @@ source $ENV_NAME/bin/activate
 pip3 install torch torchvision torchaudio
 pip install transformers datasets accelerate peft bitsandbytes ipywidgets evaluate huggingface_hub nvidia-ml-py3 
 pip install matplotlib scikit-learn pytest pre-commit torchtune
-pip install openai tiktoken trl ipykernel anthropic PyPDF2
+pip install openai tiktoken trl ipykernel anthropic PyPDF2 seaborn
 pip install git+https://github.com/pharringtonp19/trics.git
 
 echo "Setup complete and the virtual environment $ENV_NAME is ready to use."
